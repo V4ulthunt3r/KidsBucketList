b98f1e9b96c1ff2c725561d67b4c3cd64df3b942
V4ulthunt3r <peter.hildel@gmail.com>
2026-08-28 00:45:20 +0200

    Polish landing page design and delivery

---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["./*.html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        sage: "#6E8F73",
+        "sage-l": "#9CC1A2",
+        "sage-d": "#4C8A63",
+        kbg: "#F4F6F1",
+        ksurf: "#FCFEFB",
+        ktext: "#2D3B33",
+        kmuted: "#6B7F71",
+        dkbg: "#161A17",
+        dksurf: "#212723",
+        dktext: "#E8F0EA",
+        dkmuted: "#8AAD91"
+      },
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"]
+      }
+    }
+  },
+  plugins: []
+};
